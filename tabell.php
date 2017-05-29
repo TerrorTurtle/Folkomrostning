1e4ef9747bde578fd889e6f5723db6df8015993f
TerrorTurtle <te14.carl.leong@umea.nti.se>
2017-05-29 13:35:39 +0200

    igen

--- tabell.php
+++ tabell.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. "name: " $row["name"] . $row["ort"]. "" . $row["mail"]. "<br>";
+        echo "id: " . $row["id"]. "name: " $row["name"] . $row["ort"]. " " . $row["mail"]. "<br>";
     }
 } else {
     echo "0 results";
